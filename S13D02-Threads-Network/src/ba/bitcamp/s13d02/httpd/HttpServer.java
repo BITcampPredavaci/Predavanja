d26268e7d36aecf5cc985321c500a4989cd0b6d8
Damir Zekic <damirz@gmail.com>
2015-02-10 16:57:58 +0100

    Ispravak organizacije fajlova

--- S13D02-Threads-Network/src/ba/bitcamp/s13d02/httpd/HttpServer.java
+++ S13D02-Threads-Network/src/ba/bitcamp/s13d02/httpd/HttpServer.java
@@ -2,6 +2,9 @@ package ba.bitcamp.s13d02.httpd;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -12,41 +15,34 @@ import java.io.Reader;
 import java.io.Writer;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 import java.util.concurrent.ArrayBlockingQueue;
 
 /**
- * Multi-threaded ECHO server koji koristi threadpool
- * 
- * Ovaj server kreira blocking queue od max 10 elemenata u koje može smjestiti
- * otvorene konekcije, te niz od 4 thread-a koji uzimaju konekciju iz queue-a
- * kada su spremni. Ako dobijemo više hiljada konekcija momentalno, naš program
- * će još uvijek koristiti samo 4 thread-a i imati otvoreno maksimalno 14
- * konekcija u jednom momentu (10 čeka u queue-u, 4 obrađuju thread-ovi). Sve
- * ostale konekcije ostaju neotvorene dok se broj konekcija iz queue-a ne
- * smanji.
- * 
- * Broj thread-ova koji želimo koristiti u pool-u i broj konekcija koje queue
- * može čuvati zavise od potreba naše aplikacije, a prvenstveno od broja
- * očekivanih paralelnih konekcija i brzine kojom se zahtjevi izvršavaju.
+ * Jednostavan HTTTP server koji vraća zahtjevani fajl iz direktorija "http"
+ * koji se nalazi na desktopu.
  * 
  * @author damir
  *
  */
-public class EchoServer {
+public class HttpServer {
 
 	public static void main(String[] args) {
-		System.out.println("Pokrenut je ECHO server.");
-		System.out.println("Sve što server dobije će se vratiti klijentu.");
+		System.out.println("BitCamp HTTP server.");
+		System.out
+				.println("Napravite direktorij 'http' na desktopu i u njega stavite index.html fajl.");
+		System.out
+				.println("Zatim otvorite browser i upišite adresu localhost:8080/index.html.");
 		System.out.println();
 
 		// Queue svih konekcija koje držimo otvorenim dok čekam thread-ove da se
 		// oslobode
 		ArrayBlockingQueue<Socket> connections = new ArrayBlockingQueue<>(10);
 
-		Thread[] pool = { new ConnectionHandler(connections, "First"),
-				new ConnectionHandler(connections, "Second"),
-				new ConnectionHandler(connections, "Third"),
-				new ConnectionHandler(connections, "Fourth") };
+		Thread[] pool = { new HttpConnectionHandler(connections, "First"),
+				new HttpConnectionHandler(connections, "Second"),
+				new HttpConnectionHandler(connections, "Third"),
+				new HttpConnectionHandler(connections, "Fourth") };
 
 		for (Thread t : pool) {
 			t.start();
@@ -76,11 +72,26 @@ public class EchoServer {
 		System.out.println("Kraj main-a");
 	}
 
-	public static class ConnectionHandler extends Thread {
+	/**
+	 * Handler za HTTP konekcije
+	 * 
+	 * Tumači input stream ovog socketa kao Reader i čita request header-e. Ako
+	 * nađemo liniju koja počinje sa GET čitamo prvu riječ nakon nje koju
+	 * tumačimo kao naziv fajla koji klijent traži i ispisujemo sadržaj
+	 * tekstualnog fajla na output stream socketa kroz Writer.
+	 * 
+	 * Fajlove koje HTTP klijent traži ćemo pokušati pronaći unutar direktorija
+	 * "http" na Desktopu. Ako fajl ne postoji vratit ćemo status 404 Not Found
+	 * i kratku poruku o grešci.
+	 * 
+	 * @author damir
+	 *
+	 */
+	public static class HttpConnectionHandler extends Thread {
 		private ArrayBlockingQueue<Socket> connections;
 		private String name;
 
-		public ConnectionHandler(ArrayBlockingQueue<Socket> connections,
+		public HttpConnectionHandler(ArrayBlockingQueue<Socket> connections,
 				String name) {
 			super();
 			System.out.printf("[%s] Kreiran...\n", name);
@@ -99,7 +110,10 @@ public class EchoServer {
 					// u queue doda nova konekcija jedan od thread-ova će je
 					// uzeti i nastaviti izvršavanje
 					Socket connection = connections.take();
-					System.out.printf("[%s] Dobio konekciju");
+					System.out.printf("[%s] Dobio konekciju", name);
+
+					// Naziv fajla koji HTTP klijent zahtjeva
+					String requestedFileName = null;
 
 					// iz InputStream-a čitamo poruke koje klijent šalje. Pošto
 					// klijent šalje tekstualne poruke, koristit ćemo Reader da
@@ -137,8 +151,68 @@ public class EchoServer {
 						System.out.printf("[%s] Klijent rekao: %s\n", name,
 								line);
 
-						// šaljemo klijentu poruku nazad (echo)
-						pw.println(line);
+						// ako dosad nismo naišli na liniju koja počinje sa
+						// GET...
+						if (requestedFileName == null) {
+							// kreiramo scanner i gledamo prvi token linije
+							@SuppressWarnings("resource")
+							Scanner s = new Scanner(line);
+							String method = s.next();
+
+							// ako je prvi token GET, onda nam drugi token
+							// sadrži ime fajla
+							if (method.equals("GET")) {
+								requestedFileName = s.next();
+
+								// ako korisnik nije upisao ime fajla
+								// podrazumijevamo da želi fajl index.html
+								if (requestedFileName.equals("/")) {
+									requestedFileName = "/index.html";
+								}
+							}
+						}
+					}
+
+					// Sastavljamo punu putanju do fajla
+					String homePath = System.getProperty("user.home");
+					String desktopPath = homePath + "/Desktop";
+					String filePath = desktopPath + "/http" + requestedFileName;
+					System.out.printf("[%s] Pokušava otvoriti fajl %s\n", name,
+							filePath);
+
+					File f = new File(filePath);
+
+					// otvaramo reader za fajl, čitamo ga liniju po liniju i
+					// ispisujemo klijentu
+					try (Reader fileReader = new FileReader(f);
+							BufferedReader bufferedFileReader = new BufferedReader(
+									fileReader)) {
+
+						// prvo ispisujemo response header-e koji kažu da je
+						// zahtjev bio ispravan
+						pw.println("HTTP/1.1 200 OK");
+						pw.println("Content-Type: text/html; charset=utf-8");
+						pw.println();
+
+						// a zatim liniju po liniju fajla koji čitamo
+						while (true) {
+							String line = bufferedFileReader.readLine();
+							if (line == null) {
+								break;
+							}
+							pw.println(line);
+						}
+
+					} catch (FileNotFoundException e) {
+						System.out.printf(
+								"Klijent tražio fajl %s koji ne postoji.\n",
+								filePath);
+
+						// za ove slučajeve imamo grešku 404
+						pw.println("HTTP/1.1 404 Not Found");
+						pw.println("Content-Type: text/html; charset=utf-8");
+						pw.println();
+						pw.println("Nema te stranice... :(");
 					}
 
 					pw.close();

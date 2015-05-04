ca47b660f85c37ca9d986672d18fc14001812ac4
Emir Arnautovic <emir.arnautovic@atlantbh.com>
2015-05-04 13:52:33 +0200

    Changed jdoc

--- S25D02-Patterns/src/ba/bitcamp/lectures/patterns/decorator/BeverageWithChocolate.java
+++ S25D02-Patterns/src/ba/bitcamp/lectures/patterns/decorator/BeverageWithChocolate.java
@@ -1,5 +1,11 @@
 package ba.bitcamp.lectures.patterns.decorator;
 
+/**
+ * Decorates beverage with chocolate, and adds 0.7 to price.
+ * 
+ * @author emir
+ *
+ */
 public class BeverageWithChocolate extends EnchancedBeverage {
 
 	public BeverageWithChocolate(Beverage beverage) {
